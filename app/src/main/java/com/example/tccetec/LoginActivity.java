7baee7ce5cf58ecdd945a35b7328db86104bbdb3
rafaelluz10 <rafael.luztp1@gmail.com>
2024-09-18 20:17:15 -0300

    a

--- app/src/main/java/com/example/tccetec/LoginActivity.java
+++ app/src/main/java/com/example/tccetec/LoginActivity.java
@@ -20,5 +20,7 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
---
+++ app/src/main/java/com/example/tccetec/Usuario.java
@@ -0,0 +1,26 @@
+package com.example.tccetec;
+
+public class Usuario{
+    private long id;
+    private String rm;
+    public String senha;
+
+    public Usuario() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
